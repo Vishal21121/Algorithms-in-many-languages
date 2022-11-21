48ebd4f2496b8966bd741c45e17f8c57a096739e
SHREYASI DAS <104278102+Shreyasi-21@users.noreply.github.com>
2022-11-21 19:01:22 +0530

    Create Prime.java

---
+++ Prime.java
@@ -0,0 +1,23 @@
+class Prime
+{
+    public static void main (String[]args)
+    {
+
+        int num = 28, sum = 0, i = 1;
+
+        
+        while(i < num)
+        {
+           
+            if(num % i == 0)
+                sum = sum + i;
+            i++;
+        }
+
+        if (sum == num)
+            System.out.println (num + " Is a perfect number");
+        else
+            System.out.println (num + " Is not a perfect number");
+
+    }
+}
