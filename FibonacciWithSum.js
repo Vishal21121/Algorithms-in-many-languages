26df74898a56f93c871ac98063ed7ffec29d4a0b
Vishal21121 <vs84077000@gmail.com>
2022-12-07 15:05:41 +0530

    added FibonacciWithSum.js file

---
+++ FibonacciWithSum.js
@@ -0,0 +1,16 @@
+let a=0
+let b=1
+let sum=0
+let n = 10
+
+for (i = 1; i <= n; i++) {
+    sum = sum + a;
+    if (a == 0)
+        console.log(a);
+    else
+    console.log(a);
+    c = a + b;
+    a = b;
+    b = c;
+}
+console.log("\nthe sum is : " + sum);
